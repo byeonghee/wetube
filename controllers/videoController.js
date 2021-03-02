5888e50362521024f24dc2aaacbe65d7bb7ce06c
병희 임 <bhlim@stis.co.kr>
2021-03-02 17:50:57 +0900

    수정

---
+++ controllers/videoController.js
@@ -0,0 +1,10 @@
+export const home = (req,res) => res.render('Home',{pageTitle : 'Home'});
+export const search = (req,res) => { 
+    const {query :{term:searchingBy}} = req;
+    res.render('Search',{pageTitle : 'Search', searchingBy})
+};
+export const videos = (req,res) => res.render('Videos',{pageTitle : 'Videos'});
+export const upload = (req,res) => res.render('Upload',{pageTitle : 'Upload'});
+export const videoDetail = (req,res) => res.render('Video Detail',{pageTitle : 'Video Detail'});
+export const editVideo = (req,res) => res.render('Edit Video',{pageTitle : 'Edit Video'});
+export const deleteVideo = (req,res) => res.render('Delete Video',{pageTitle : 'Delete Video'});
